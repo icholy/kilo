bb00e46d667d939ee7964654149bdc26d86666b7
Ilia Choly <ilia.choly@gmail.com>
2022-11-16 20:27:11 -0500

    setup and restore the termios

--- main.go
+++ main.go
@@ -8,22 +8,33 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func enableRawMode() error {
+func enableRawMode() (*unix.Termios, error) {
 	raw, err := unix.IoctlGetTermios(0, unix.TCGETS)
 	if err != nil {
-		return fmt.Errorf("failed to get termios: %v", err)
+		return nil, fmt.Errorf("failed to get termios: %v", err)
 	}
+	original := *raw
 	raw.Lflag &^= unix.ECHO
 	if err := unix.IoctlSetTermios(0, unix.TCSETS, raw); err != nil {
-		return fmt.Errorf("failed to set termios: %v", err)
+		return nil, fmt.Errorf("failed to set termios: %v", err)
+	}
+	return &original, nil
+}
+
+func restoreMode(state *unix.Termios) error {
+	if err := unix.IoctlSetTermios(0, unix.TCSETS, state); err != nil {
+		return fmt.Errorf("failed to restore termios: %v", err)
 	}
 	return nil
 }
 
 func main() {
-	if err := enableRawMode(); err != nil {
+	// raw mode
+	state, err := enableRawMode()
+	if err != nil {
 		log.Fatal(err)
 	}
+	defer restoreMode(state)
 	// byte reader loop
 	b := make([]byte, 1)
 	for {

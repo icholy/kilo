82296709c4b725d63d3c8a04e61c4e52e44f34c5
Ilia Choly <ilia.choly@gmail.com>
2022-11-16 21:17:45 -0500

    refactor to be closer to source material

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 
 	"golang.org/x/sys/unix"
 )
 
-func enableRawMode() (*unix.Termios, error) {
+var originalRaw *unix.Termios
+
+func enableRawMode() {
 	raw, err := unix.IoctlGetTermios(0, unix.TCGETS)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get termios: %v", err)
+		log.Fatalf("failed to get termios: %v", err)
 	}
-	original := *raw
+	clone := *raw
+	originalRaw = &clone
 	raw.Iflag &^= unix.BRKINT | unix.ICRNL | unix.INPCK | unix.ISTRIP | unix.IXON
 	raw.Oflag &^= unix.OPOST
 	raw.Cflag &^= unix.CS8
@@ -21,16 +23,21 @@ func enableRawMode() (*unix.Termios, error) {
 	raw.Cc[unix.VMIN] = 0
 	raw.Cc[unix.VTIME] = 1
 	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, raw); err != nil {
-		return nil, fmt.Errorf("failed to set termios: %v", err)
+		log.Fatalf("failed to set termios: %v", err)
 	}
-	return &original, nil
 }
 
-func restoreMode(state *unix.Termios) error {
-	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, state); err != nil {
-		return fmt.Errorf("failed to restore termios: %v", err)
+func restoreMode() {
+	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, originalRaw); err != nil {
+		log.Fatalf("failed to restore termios: %v", err)
 	}
-	return nil
+}
+
+func die(format string, args ...any) {
+	editorRefreshScreen()
+	msg := fmt.Sprintf(format, args...)
+	unix.Write(unix.Stdout, []byte(msg))
+	unix.Exit(0)
 }
 
 func controlKey(c byte) byte {
@@ -45,50 +52,33 @@ func editorReadKey() byte {
 			return b[0]
 		}
 		if n == -1 && err != unix.EAGAIN {
-			log.Fatalf("failed to read byte: %v", err)
+			die("read: %v", err)
 		}
 	}
 }
 
-var ErrExit = errors.New("exit")
-
-func editorProcessKeypress() error {
+func editorProcessKeypress() {
 	c := editorReadKey()
 	switch c {
 	case controlKey('q'):
-		return ErrExit
+		editorRefreshScreen()
+		restoreMode()
+		unix.Exit(0)
 	}
-	return nil
 }
 
-func editorRefreshScreen() error {
-	if _, err := unix.Write(unix.Stdout, []byte("\x1b[2J")); err != nil {
-		return err
-	}
-	if _, err := unix.Write(1, []byte("\x1b[H")); err != nil {
-		return err
-	}
-	return nil
+func editorRefreshScreen() {
+	unix.Write(unix.Stdout, []byte("\x1b[2J"))
+	unix.Write(unix.Stdout, []byte("\x1b[H"))
 }
 
 func main() {
 	// raw mode
-	state, err := enableRawMode()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer restoreMode(state)
+	enableRawMode()
+	defer restoreMode()
 	// byte reader loop
 	for {
-		if err := editorRefreshScreen(); err != nil {
-			log.Fatalf("refresh screen: %v", err)
-		}
-		err := editorProcessKeypress()
-		if err == ErrExit {
-			break
-		}
-		if err != nil {
-			log.Fatalf("process keypress: %v", err)
-		}
+		editorRefreshScreen()
+		editorProcessKeypress()
 	}
 }

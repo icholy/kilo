3ce95d0e9c818412e3ace4eb0632bd65d624fb4d
Ilia Choly <ilia.choly@gmail.com>
2022-11-16 20:23:07 -0500

    don't use the x/term package

--- main.go
+++ main.go
@@ -4,31 +4,32 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"unicode"
 
-	"golang.org/x/term"
+	"golang.org/x/sys/unix"
 )
 
-func main() {
-	// enter raw mode
-	state, err := term.MakeRaw(0)
+func enableRawMode() error {
+	raw, err := unix.IoctlGetTermios(0, unix.TCGETS)
 	if err != nil {
+		return fmt.Errorf("failed to get termios: %v", err)
+	}
+	raw.Lflag &^= unix.ECHO
+	if err := unix.IoctlSetTermios(0, unix.TCSETS, raw); err != nil {
+		return fmt.Errorf("failed to set termios: %v", err)
+	}
+	return nil
+}
+
+func main() {
+	if err := enableRawMode(); err != nil {
 		log.Fatal(err)
 	}
-	// restore state
-	defer term.Restore(0, state)
 	// byte reader loop
 	b := make([]byte, 1)
 	for {
 		n, _ := os.Stdin.Read(b)
 		if n != 1 || b[0] == 'q' {
 			break
-		}
-		c := b[0]
-		if unicode.IsPrint(rune(c)) {
-			fmt.Printf("%d ('%c')\r\n", c, c)
-		} else {
-			fmt.Printf("%d\r\n", c)
 		}
 	}
 }

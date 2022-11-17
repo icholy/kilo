6234d4082c564ad58bdcbb8cf5c4cdd89c0abb55
Ilia Choly <ilia.choly@gmail.com>
2022-11-16 19:56:51 -0500

    enter raw mode and print bytes

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/icholy/kilo
 
 go 1.19
+
+require (
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
+	"unicode"
+
+	"golang.org/x/term"
 )
 
 func main() {
+	// enter raw mode
+	state, err := term.MakeRaw(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// restore state
+	defer term.Restore(0, state)
+	// byte reader loop
 	b := make([]byte, 1)
 	for {
 		n, _ := os.Stdin.Read(b)
 		if n != 1 || b[0] == 'q' {
 			break
+		}
+		c := b[0]
+		if unicode.IsPrint(rune(c)) {
+			fmt.Printf("%d ('%c')\n", c, c)
+		} else {
+			fmt.Printf("%d\n", c)
 		}
 	}
 }
